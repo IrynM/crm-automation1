9416e19f73252255a7edb05e82fa23e1524655e6
aziza <azuzya26@gmail.com>
2022-02-28 08:33:02 -0600

    set up environment

--- src/test/java/com/nextbasecrm/tests/US_10_CreateTask.java
+++ src/test/java/com/nextbasecrm/tests/US_10_CreateTask.java
@@ -1,4 +1,27 @@
 package com.nextbasecrm.tests;
 
+import com.nextbasecrm.utilities.ConfigurationReader;
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
 public class US_10_CreateTask {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+        String browserType = ConfigurationReader.getProperty("browser");
+        String env= ConfigurationReader.getProperty("env");
+        driver = WebDriverFactory.getDriver(browserType);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get(env);
+    }
+
+
+
 }
