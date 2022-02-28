23bcc3402114a2a8649d2b2e811cfb0811782f4a
aziza <azuzya26@gmail.com>
2022-02-28 08:40:55 -0600

    pop-up message when created task for username 1 and 2

--- src/test/java/com/nextbasecrm/tests/US_10_CreateTask.java
+++ src/test/java/com/nextbasecrm/tests/US_10_CreateTask.java
@@ -55,6 +55,63 @@ public class US_10_CreateTask {
         String expectedPopUpText="Task has been created";
         Assert.assertEquals(actualPopupText,expectedPopUpText);
 
+    }
+    //Once a task is created successfully, there should be a confirmation message
+    //dimply in a popup. “Task has been created”
+    @Test
+    public void popUpMessage1() {
+
+        String eachUserName = ConfigurationReader.getProperty("username1");
+        String password =ConfigurationReader.getProperty("password");
+        CRM_Utilities.crm_login(driver, eachUserName, password);
+        WebElement taskTab = driver.findElement(By.xpath("//div[@class ='feed-add-post-form-variants']//span[2]"));
+        taskTab.click();
+
+        WebElement iframe = driver.findElement(By.xpath("//div[@id='bx-html-editor-iframe-cnt-lifefeed_task_form']/iframe"));
+        driver.switchTo().frame(iframe);
+        BrowserUtils.sleep(3);
+        WebElement taskContentWindow = driver.findElement(By.xpath("//body[@contenteditable='true']"));
+        taskContentWindow.sendKeys("Aziza automated task 30");
+        driver.switchTo().defaultContent();
+        WebElement titleContentEdit = driver.findElement(By.xpath("//input[@data-bx-id='task-edit-title']"));
+        titleContentEdit.sendKeys("TaskCase23 automation");
+        WebElement sendButtonToSaveTask = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
+        sendButtonToSaveTask.click();
+
+        WebElement taskCreatedPopup= driver.findElement(By.xpath("//div[@class='feed-create-task-popup-title']"));
+        String actualPopupText= taskCreatedPopup.getText();
+        String expectedPopUpText="Task has been created";
+        Assert.assertEquals(actualPopupText,expectedPopUpText);
+
+
+    }
+    //Once a task is created successfully, there should be a confirmation message
+    //dimply in a popup. “Task has been created”
+    @Test
+    public void popUpMessage2() {
+
+        String eachUserName = ConfigurationReader.getProperty("username2");
+        String password =ConfigurationReader.getProperty("password");
+        CRM_Utilities.crm_login(driver, eachUserName, password);
+        WebElement taskTab = driver.findElement(By.xpath("//div[@class ='feed-add-post-form-variants']//span[2]"));
+        taskTab.click();
+
+        WebElement iframe = driver.findElement(By.xpath("//div[@id='bx-html-editor-iframe-cnt-lifefeed_task_form']/iframe"));
+        driver.switchTo().frame(iframe);
+        BrowserUtils.sleep(3);
+        WebElement taskContentWindow = driver.findElement(By.xpath("//body[@contenteditable='true']"));
+        taskContentWindow.sendKeys("Aziza automated task 30");
+        driver.switchTo().defaultContent();
+        WebElement titleContentEdit = driver.findElement(By.xpath("//input[@data-bx-id='task-edit-title']"));
+        titleContentEdit.sendKeys("TaskCase23 automation");
+        WebElement sendButtonToSaveTask = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
+        sendButtonToSaveTask.click();
+
+        WebElement taskCreatedPopup= driver.findElement(By.xpath("//div[@class='feed-create-task-popup-title']"));
+        String actualPopupText= taskCreatedPopup.getText();
+        String expectedPopUpText="Task has been created";
+        Assert.assertEquals(actualPopupText,expectedPopUpText);
+
 
     }
 
